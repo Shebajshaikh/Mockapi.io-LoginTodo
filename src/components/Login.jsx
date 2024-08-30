d573fa44fa6fca7d97f464cdd9300f554e6632c1
Shebajshaikh <MDSHEBAJ2024@GMAIL.COM>
2024-08-30 23:37:27 +0530

    Convert RTK

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -22,11 +22,15 @@ function Login({ onLogin }) {
       <form onSubmit={handleSubmit}>
         <label>
           Email:
-          <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} required />
+          <input type="text"
+           value={email} 
+           onChange={(e) => setEmail(e.target.value)} required />
         </label>
         <label>
           Password:
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+          <input type="password"
+           value={password} 
+           onChange={(e) => setPassword(e.target.value)} required />
         </label>
         <button type="submit">
           Login
